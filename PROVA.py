dddf47ac6b45c36f58f80cfe15440ff3da4e5946
loren <lorenzo.lospennato@epfl.ch>
2023-02-03 17:06:42 +0100

    first_try

--- PROVA.py
+++ PROVA.py
@@ -504,9 +504,148 @@ import math
 # hello()
 # bye()
 
-from messages import *  #it means import all
-hello()
-bye()
+# from messages import *  #it means import all
+# hello()
+# bye()
+
+# help("modules")
+
+
+#CREATION OF A BASIC GAME (sasso, carta, forbice)
+# import random
+# while True:
+#     choices = ["rock", "paper", "scissors"]
+#     player = None
+#     computer = random.choice(choices)
+#     while player not in choices:
+#         player = input("rock, paper or scissors?: ").lower()
+#
+#     if player==computer:
+#         print("player: ",player)
+#         print("computer: ",computer)
+#         print("Tie!")
+#     elif player=='rock':
+#         if computer=="paper":
+#             print("player: ", player)
+#             print("computer: ", computer)
+#             print("You lose")
+#         if computer=="scissors":
+#             print("player: ", player)
+#             print("computer: ", computer)
+#             print("You win")
+#     elif player=='scissors':
+#         if computer=="paper":
+#             print("player: ", player)
+#             print("computer: ", computer)
+#             print("You win")
+#         if computer=="rock":
+#             print("player: ", player)
+#             print("computer: ", computer)
+#             print("You lose")
+#     elif player=='paper':
+#         if computer=="scissors":
+#             print("player: ", player)
+#             print("computer: ", computer)
+#             print("You lose")
+#         if computer=="rock":
+#             print("player: ", player)
+#             print("computer: ", computer)
+#             print("You win")
+#     play_again=input("Play again? (yes/no): ").lower()
+#
+#     if play_again!="yes":
+#         break
+# print("BYE")
+
+#GAME QUIZ GAME
+
+#-------------
+def new_game():
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+    check = 0
+
+    for key in questions:
+        print("--------------------------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        while check!="True":
+            guess = input("Enter (A, B, C, D): ")
+            guess = guess.upper()
+            if guess != "A":
+                guess = input("Enter (A, B, C, D): ")
+            elif guess != "B":
+                guess = input("Enter (A, B, C, D): ")
+            elif guess != "C":
+                guess = input("Enter (A, B, C, D): ")
+            elif guess != "D":
+                guess = input("Enter (A, B, C, D): ")
+            else:
+                check = 1
+
+        guesses.append(guess) #insert the guess in the before created list
+
+        correct_guesses += check_answer(questions.get(key),guess) #it takes the results from questions on the same position of key
+        question_num +=1
+
+    display_score(correct_guesses, guesses)
+
+#-----------------
+def check_answer(answer, guess):
+    if answer==guess:
+        print("Correct")
+        return 1
+    else:
+        print("Wrong")
+        return 0
+
+#-------------------
+def display_score(correct_guesses, guesses):
+    print("---------------------------")
+    print("RESULTS")
+    print("---------------------------")
+    print("Answers: ", end=" ")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print() #it is used to skip a line
+
+    print("Guesses: ", end=" ")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score = int((correct_guesses/len(questions))*100)
+    print("Your score is "+str(score)+"%")
+#------------------
+def play_again():
+
+    response = input("Do you want to play again? (yes or no): ")
+    response= response.upper()
+
+    if response == "YES":
+        return True
+    else:
+        return False
+
+#we use a dictionary in order to create a quiz game
+questions={
+    "Who created python?: ": "A",
+    "What year was Python created?: ": "B",
+    "Python is tributed to which comedy group?: " : "C",
+    "Is the Earth round? :" : "A"
+}
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
+           ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+           ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+           ["A. True", "B. False", "C. sometimes", "D. What's Earth?"]]
 
 
 
+new_game()
+
+while play_again():
+    new_game()
+
+print("Bye")
