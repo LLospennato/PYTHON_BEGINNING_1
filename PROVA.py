bf3f8a3554c09e9df552c63fd0e3e76a7e974587
loren <lorenzo.lospennato@epfl.ch>
2023-02-06 11:51:02 +0100

    first_try

--- PROVA.py
+++ PROVA.py
@@ -560,92 +560,511 @@ import math
 #GAME QUIZ GAME
 
 #-------------
-def new_game():
-    guesses = []
-    correct_guesses = 0
-    question_num = 1
-    check = 0
+# def new_game():
+#     guesses = []
+#     correct_guesses = 0
+#     question_num = 1
+#     call = 0
+#     for key in questions:
+#         print("--------------------------------------------")
+#         print(key)
+#         for i in options[question_num-1]:
+#             print(i)
+#         while call==0:
+#             guess = input("Enter (A, B, C, D): ")
+#             guess = guess.upper()
+#             if guess == "A" or guess == "B" or guess == "C" or guess == "D":
+#                 call = 1
+#         call = 0
+#
+#         guesses.append(guess) #insert the guess in the before created list
+#         correct_guesses += check_answer(questions.get(key),guess) #it takes the results from questions on the same position of key
+#         question_num +=1
+#
+#     display_score(correct_guesses, guesses)
+#
+# #-----------------
+# def check_answer(answer, guess):
+#     if answer==guess:
+#         print("Correct")
+#         return 1
+#     else:
+#         print("Wrong")
+#         return 0
+#
+# #-------------------
+# def display_score(correct_guesses, guesses):
+#     print("---------------------------")
+#     print("RESULTS")
+#     print("---------------------------")
+#     print("Answers: ", end=" ")
+#     for i in questions:
+#         print(questions.get(i), end=" ")
+#     print() #it is used to skip a line
+#
+#     print("Guesses: ", end=" ")
+#     for i in guesses:
+#         print(i, end=" ")
+#     print()
+#
+#     score = int((correct_guesses/len(questions))*100)
+#     print("Your score is "+str(score)+"%")
+# #------------------
+# def play_again():
+#
+#     response = input("Do you want to play again? (yes or no): ")
+#     response= response.upper()
+#
+#     if response == "YES":
+#         return True
+#     else:
+#         return False
+#
+#
+#
+# #we use a dictionary in order to create a quiz game
+# questions={
+#     "Who created python?: ": "A",
+#     "What year was Python created?: ": "B",
+#     "Python is tributed to which comedy group?: " : "C",
+#     "Is the Earth round? :" : "A"
+# }
+# options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
+#            ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+#            ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+#            ["A. True", "B. False", "C. sometimes", "D. What's Earth?"]]
+#
+#
+#
+# new_game()
+#
+# while play_again():
+#     new_game()
+#
+# print("Bye")
 
-    for key in questions:
-        print("--------------------------------------------")
-        print(key)
-        for i in options[question_num-1]:
-            print(i)
-        while check!="True":
-            guess = input("Enter (A, B, C, D): ")
-            guess = guess.upper()
-            if guess != "A":
-                guess = input("Enter (A, B, C, D): ")
-            elif guess != "B":
-                guess = input("Enter (A, B, C, D): ")
-            elif guess != "C":
-                guess = input("Enter (A, B, C, D): ")
-            elif guess != "D":
-                guess = input("Enter (A, B, C, D): ")
-            else:
-                check = 1
+#PYTHON OBJECT ORIENTED PROGRAMMING
+#It is better to create the class in another file and import it in the current script
 
-        guesses.append(guess) #insert the guess in the before created list
+# from car import Car #from the name of the file import the class
+#
+# car_1 = Car("Chevy", "Corvette", 2021, "Blue") # in this way we create an object from the defined class
+# car_2 = Car("Ford", "Mustang", 2022, "Red")
+# print(car_2.make)
+# print(car_2.model)
+# print(car_2.year)
+# print(car_2.color)
+#
+# car_1.drive()
+# car_2.stop()
 
-        correct_guesses += check_answer(questions.get(key),guess) #it takes the results from questions on the same position of key
-        question_num +=1
+#CLASS VARIABLES
 
-    display_score(correct_guesses, guesses)
+# from car import Car
+# car_1 = Car("Chevy", "Corvette", 2021, "Blue")
+# car_2 = Car("Ford", "Mustang", 2022, "Red")
+# #
+# # car_1.wheels=2 #to change it only for car_1
+# #
+# # print(car_1.wheels)
+# Car.wheels=2 #it changes the value of the class--- it effects all the instances of the class
+# print(Car.wheels) #number of wheels for that class
+#
+# print(car_1.wheels)
+# print(car_2.wheels)
 
-#-----------------
-def check_answer(answer, guess):
-    if answer==guess:
-        print("Correct")
-        return 1
-    else:
-        print("Wrong")
-        return 0
+#INHERITANCE IN PYTHON --->inherit from other classes
+#Thi is better when we have many different classes with the same features
+#PARENT CLASS
+# class Animal:
+#     alive = True
+#
+#     def eat(self):
+#         print("This animal is eating")
+#
+#     def sleep(self):
+#         print("This animal is sleeping")
+#
+# #FAMILY TREE -- they can also have their own characteristics
+# class Rabbit(Animal): #This is a "children" class--> we have to pass into the parentesis the name of the other class
+#     def run(self):
+#         print("This rabbit is running")
+#
+# class Fish(Animal):
+#     def swim(self):
+#         print("This fish is swimming")
+#
+# class Hawk(Animal):
+#     def fly(self):
+#         print("This hawk is flying")
+#
+# rabbit = Rabbit()
+# fish = Fish()
+# hawk = Hawk()
+#
+# print(rabbit.alive) #they have the class variable alive even if it is empty because they inherited what is in animal
+# fish.eat()
+# hawk.sleep()
+#
+# rabbit.run()
+# fish.swim()
+# hawk.fly()
 
-#-------------------
-def display_score(correct_guesses, guesses):
-    print("---------------------------")
-    print("RESULTS")
-    print("---------------------------")
-    print("Answers: ", end=" ")
-    for i in questions:
-        print(questions.get(i), end=" ")
-    print() #it is used to skip a line
+#MULTI LEVEL INHERITANCE --> a derived (child) class inherit from another derived class
 
-    print("Guesses: ", end=" ")
-    for i in guesses:
-        print(i, end=" ")
-    print()
+# class Organism:
+#     alive = True
+#
+# class Animal(Organism):
+#
+#     def eat(self):
+#         print("This animal is eating")
+#
+# class Dog(Animal):
+#     def bark(self):
+#         print("This dog is barking")
+#
+# dog = Dog()
+# print(dog.alive)
+# dog.eat()
+# dog.bark()
 
-    score = int((correct_guesses/len(questions))*100)
-    print("Your score is "+str(score)+"%")
-#------------------
-def play_again():
+#MULTIPLE INHERITANCE ---> a child class is derived from more that one parent class
 
-    response = input("Do you want to play again? (yes or no): ")
-    response= response.upper()
+# class Prey:
+#
+#     def flee(self):
+#         print("This animal flees")
+#
+# class Predator:
+#
+#     def hunt(self):
+#         print("This animal is hunting")
+#
+# class Rabbit(Prey):
+#     pass
+# class Hawk(Predator):
+#     pass
+# class Fish(Prey, Predator):  #it has access to both the classes
+#     pass
+#
+# rabbit = Rabbit()
+# hawk = Hawk()
+# fish = Fish()
+#
+# rabbit.flee()
+# hawk.hunt()
+# fish.hunt()
+# fish.flee()
 
-    if response == "YES":
-        return True
-    else:
-        return False
+#METHOD OVER-WRITING
 
-#we use a dictionary in order to create a quiz game
-questions={
-    "Who created python?: ": "A",
-    "What year was Python created?: ": "B",
-    "Python is tributed to which comedy group?: " : "C",
-    "Is the Earth round? :" : "A"
-}
-options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
-           ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
-           ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
-           ["A. True", "B. False", "C. sometimes", "D. What's Earth?"]]
+# class Animal:
+#
+#     def eat(self):
+#         print("This animal is eating")
+#
+# class Rabbit(Animal):
+#
+#     def eat(self): # to overwrite we need to define the method with the same parameters
+#         print("This rabbit is eating a carrot") #now we will use this version for the eat method
+# rabbit = Rabbit()
+# rabbit.eat()
+
+#METHOD CHAINING ---->calling method sequentially
+#each call performs an action on the same object and return self
+
+# class Car: #we need to return something
+#     def turn_on(self):
+#         print("you start the engine")
+#         return self
+#     def drive(self):
+#         print("You drive the car")
+#         return self
+#     def brake(self):
+#         print("You step on the brakes")
+#         return self
+#     def turn_off(self):
+#         print("You turn off the engine")
+#         return self
+#
+# car = Car()
+
+# car.turn_on().drive()
+# car.drive()
+# car.brake().turn_off()
+# car.turn_on().drive().brake().turn_off()
+
+
+#SUPER FUNCTION --> super(); it is used to give access to the methods of a parent class. It returns a temporary object
+#                            of a parent class when used
+
+# class Rectangle:  # any similarities could be placed inside the parent class in order not to repeat the same lines of code
+#
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
+#
+# class Square(Rectangle):
+#
+#     def __init__(self, length, width):
+#         super().__init__(length,width)
+#     def area(self):
+#         return self.length*self.width
+# class Cube(Rectangle):
+#     def __init__(self, length, width, height):
+#         super().__init__(length,width)
+#         self.height = height
+#     def volume(self):
+#         return self.length*self.width*self.height
+#
+# square = Square(3,3)
+# cube = Cube (3,3,3)
+#
+# print(square.area())
+# print(cube.volume())
+
+
+#ABSTRACT CLASSES
+#It preventes a user from creating an object of that class
+# + compels a user to override abstract methods in a child class
+
+# from abc import ABC,abstractmethod  #used to prevent to create a vehicle object
+# class Vehicle(ABC):
+#     @abstractmethod
+#     def go(self):
+#         pass
+#     @abstractmethod
+#     def stop(self):
+#         pass
+#
+# #For the child classes we need to implement all the abstract methods from the parent class
+# class Car(Vehicle):
+#
+#     def go(self):
+#         print("You drive the car")
+#     def stop(self):
+#         print("This car is stopped")
+# class Motorcycle(Vehicle):
+#
+#     def go(self):
+#         print("You ride the motorcycle")
+#     def stop(self):
+#         print("This motorcycle is stopped")
+# # vehicle = Vehicle()  #we cannot create this object
+# car = Car()
+# motorcycle = Motorcycle()
+#
+# # vehicle.go()
+# car.go()
+# motorcycle.go()
+# car.stop()
+# motorcycle.stop()
 
 
 
-new_game()
+#PASS OBJECTS AS ARGUMENTS
 
-while play_again():
-    new_game()
+# class Car:
+#
+#     color = None
+#
+# class Motorcycle:
+#
+#     color = None
+#
+# # If we want do define a separate function it needs to be outside the class
+# def change_color(vehicle, color):  #we accept a car object and a color
+#     vehicle.color = color
+#
+# # We define the objects linked to the class
+# car_1 = Car()
+# car_2 = Car()
+# car_3 = Car()
+#
+# bike_1 = Motorcycle()
+#
+#
+# change_color(car_1, "red")
+# change_color(car_2, "white")
+# change_color(car_3, "blue")
+# change_color(bike_1,"black")
+#
+# print(car_1.color)
+# print(car_2.color)
+# print(car_3.color)
+# print(bike_1.color)
 
-print("Bye")
+#DUCK TYPING: concept where the class of an object is less important that the methods/attributes
+#             class type is not checked if minimum methods/attributes are present
+#             "If it walks like a duck, and it quacks like a duck, then it must be a duck."
+
+# class Duck:
+#
+#     def walk(self):
+#         print("This duck is walking")
+#
+#     def talk(self):
+#         print("This duck is qwuacking")
+#
+# class Chicken:
+#     def walk(self):
+#         print("This chicken is walking")
+#     def talk(self):
+#         print("This chicken is clucking")
+#
+# class Person():
+#
+#     def catch(self,duck):
+#         duck.walk()
+#         duck.talk()
+#         print("You caught the critter!")
+#
+# duck = Duck()
+# chicken = Chicken()
+# person = Person()
+#
+# person.catch(chicken)
+
+
+
+#WALRUS OPERATOR :=
+# It assigns values to variables as part of a larger expression
+
+# happy = True
+# print(happy)
+
+# print(happy := True)
+
+# foods = list()
+# while True:
+#     food = input("What food do you like?: ")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+
+#USING A WALRUS OPERATOR (in less lines)
+# foods = list()
+# while food := input("What food do you like?: ") != "quit":
+#     foods.append(food)
+
+
+
+#ASSIGN A FUNCTION TO A VARIABLE
+# def hello():
+#     print("Hello")
+#
+# print(hello) # It prints the memory address of the function in the memory
+# hi = hello
+# # print(hi)
+# hi() # As calling the hello function
+
+# say = print
+# say("Whooooooo") # It prints, since it does the same stuff of a print function
+
+#HIGHER ORDER FUNCTION: a function that either:
+#                       1) accepts a function as an argument
+#                           or
+#                       2) returns a function  (in python, functions are also treated as objects)
+
+# def loud(text):
+#     return text.upper()
+#
+# def quiet(text):
+#     return text.lower()
+#
+# def hello(func):
+#     text = func("Hello")
+#     print(text)
+#
+# hello(loud) #I'm passing a function as argument
+# hello(quiet)
+#
+#
+# def divisor(x):
+#     def dividend(y):
+#         return y / x
+#     return dividend
+#
+# divide = divisor(2) # we are returning the dividend and assign it to a variable
+# print(divide(10)) # we can call a variable if it has a memory adress of a function
+
+
+#LAMBDA FUNCTION: function written in 1 line using lambda keyword
+#                 accepts any number of arguments, but only has one expression.
+#                 (think of it as a shortcut)
+#                 (useful if needed for a short period of time, throw-away)
+#lambda parameters:expression  SINTAX
+
+# def double(x):
+#     return x*2
+#
+# print(double(5))
+
+# double = lambda x: x*2 #lambda parameters : expression
+# print(double(5))
+
+# multiply = lambda x,y : x * y
+# print(multiply(6,5))
+
+# add= lambda x,y,z : x+y+z
+# print(add(1,2,3))
+
+# full_name=lambda first_name, last_name: first_name+" "+last_name
+# print(full_name("Lorenzo","Lospennato"))
+
+# age_check = lambda age:True if age>=18 else False
+# print(age_check(17))
+
+#SORT ITERABLES
+#sort() method = used with lists
+#sort() function = used with iterables
+#level 1
+# students = ("Squidward", "Sandy", "Patrick", "Spongebob", "Mr.Krabs")
+#
+# # students.sort(reverse=True) #Reverse it only works with lists
+# sorted_students = sorted(students, reverse=True)
+#
+#
+# for i in sorted_students:
+#     print(i)
+
+#level 2 how to sort in function of name, grade or age ----list of tuples
+# students =[("Squidward","F", 60),("Sandy","A",33),("Patrick","D",36),("Spongebob","B",20),("Mr.Krabs","C",78)]
+# students =(("Squidward","F", 60),("Sandy","A",33),("Patrick","D",36),("Spongebob","B",20),("Mr.Krabs","C",78))
+# #sorting by name
+# # students.sort()
+# #
+# # grade = lambda grades:grades[1] #we take the second column (grades)
+# # students.sort(key=grade, reverse=True) #grade is a function object via a lambda function ----key return the index of the column we need
+#
+# age = lambda ages:ages[2] #we take the second column (grades)
+# # students.sort(key=age, reverse=True)
+# sorted_students = sorted(students, key=age) #in the case of a tuple of tuples we cannot use sort directly but a function
+# for i in sorted_students:
+#     print(i)
+
+
+#MAP FUNCTION map()
+# It applies a function to each item in an iterable (list, tuple, etc.)
+# map(function, iterable)
+
+# store = [("shirt", 20.00),("pants", 25.00),("jacket",50.00),("socks",10.00)]
+#
+# #convert prices from dollar to euros
+# # to_euros = lambda data : (data[0],data[1]*0.82) #parentesis in order to create a tuple and then we take what we want
+# to_dollars = lambda data : (data[0],data[1]/0.82)
+#
+# # store_euros = list(map(to_euros, store))
+# store_dollars = list(map(to_dollars, store))
+# for i in store_dollars:
+#     print(i)
+
+
+
+#FILTER FUNCTION filter()
+# It creates a collection of elements from an iterable for which a function return True
+#filter(function,iterable)
+
+friends = [("Rachel",19),("Monica",18),("Phoebe",17),("Joey",16),("Chandler",21),("Ross",20)]
+
